68224b40ba8e8800ecb43e243d43197819759c07
James Lewis <james.will.lewis@gmail.com>
2012-09-04 21:02:49 +0200

    Added serial version UID numbers to serializable classes.

--- WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/message/BidMessage.java
+++ WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/message/BidMessage.java
@@ -2,4 +2,9 @@ package com.uct.cs.wsintelliauction.network.message;
 
 public class BidMessage extends Message {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5476369529938686145L;
+
 }
--- WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/message/InfoMessage.java
+++ WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/message/InfoMessage.java
@@ -2,6 +2,9 @@ package com.uct.cs.wsintelliauction.network.message;
 
 public class InfoMessage extends Message{
 
+
+	private static final long serialVersionUID = 1988739302576718985L;
+
 	private String data;
 	
 	public InfoMessage(String data) {
