06280eae684d0a2d58483d62c389f9f96c9aa584
James Lewis <james.will.lewis@gmail.com>
2012-09-04 16:28:56 +0200

    Added: Thread pool for multiple client connection handling and set maximum number of connected clients.

--- WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/ServerNetworkInterface.java
+++ WSIntelliAuction/src/com/uct/cs/wsintelliauction/network/ServerNetworkInterface.java
@@ -3,6 +3,8 @@ package com.uct.cs.wsintelliauction.network;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerNetworkInterface extends NetworkInterface {
 
@@ -10,10 +12,19 @@ public class ServerNetworkInterface extends NetworkInterface {
 	 * Arraylist of client connections wrapped in a synchronized container.
 	 */
 	private List<NetworkConnection> clientObjects;
+	/**
+	 * Pool of worker threads to hande each recipient socket
+	 */
+	private ExecutorService clientThreads; 
+	/**
+	 * 
+	 */
+	public final int MAX_CLIENT_CONNECTIONS = 32;
 
 	public ServerNetworkInterface() {
 		clientObjects = Collections
 				.synchronizedList(new ArrayList<NetworkConnection>());
+		clientThreads = Executors.newFixedThreadPool(MAX_CLIENT_CONNECTIONS);
 	}
 	
 	
